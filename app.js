be2f4793cb1b5713555d2c0b7aa70107f9b21023
Karunesh123 <65278651+Karunesh123@users.noreply.github.com>
2020-05-16 16:53:29 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+(function (){
+ 'user strict';
+
+angular.module("LunchCheck",[])
+.controller("LunchCheckController",LunchCheckController)
+
+LunchCheckController.$inject=["$scope","$filter"];
+
+function LunchCheckController($scope,$filter) {
+
+ $scope.store="";
+ $scope.message = function()
+ {
+    var str = $scope.store.split(',');
+
+    if($scope.store=="")
+    {
+
+             $scope.temp = "Please enter data first";
+
+
+}
+        else if(str.length<=3)
+       {
+           $scope.temp = "Enjoy!";
+       }
+
+
+   else {
+     $scope.temp =" Too much!";
+   }
+
+};
+
+}
+
+})();
